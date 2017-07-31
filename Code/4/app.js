119e776897eb385fa55a3ccad4a34a9e8614e548
Dhaval.Patel <dhaval.patel@rishabhsoft.com>
2017-07-31 15:03:52 +0530

    Exercise 4,5 completed.

--- Code/4/app.js
+++ Code/4/app.js
@@ -4,14 +4,33 @@ new Vue({
     divEffect: {
       highlight: true,
       shrink: false
-    }
+    },
+    color: {
+      'bg-color': true
+    },
+    colorClass: "color",
+    divClass: 'red',
+    div2Class: '',
+    div3Class: '',
+    defaultClass: false,
+    progress: 0
   },
   methods: {
     startEffect: function() {
-    
+      const self = this;
+      setInterval(function(){
+        self.divEffect.highlight = !self.divEffect.highlight;
+        self.divEffect.shrink = !self.divEffect.shrink;
+      }, 1000);
     },
     startProgress: function() {
-      
+      const self = this;
+      const interval = setInterval(function(){
+        self.progress++;
+        if(self.progress == 100) {
+            clearInterval(interval);
+        }
+      }, 10);
     }
   }
 });
